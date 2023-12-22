9c840137cbee7f7225115809d548a08d384f0c79
MayaS1111 <113066754+MayaS1111@users.noreply.github.com>
2023-12-21 21:18:04 -0600

    Created check out page, checkout items,and checkout cart funtionality

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,7 +2,7 @@ import {Fragment, useContext} from 'react'
 import { Outlet, Link } from 'react-router-dom';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
-import CartDropdown from '../../components/cart-dropdown/car-dropdown.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import { CartContext } from '../../contexts/cart.context';
 import { UserContext } from '../../contexts/user.context';
